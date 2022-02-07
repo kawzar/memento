a847c57171d4712151bd71dd3e0130b3f0021354
Marlow Haspert <marlowhaspert@gmail.com>
2022-02-06 23:50:47 -0300

    cha cha cha cha changes changes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,16 @@ public class Player : MonoBehaviour
     public static Player Instance { get; private set; }
 
     [SerializeField]
-    private NavMeshAgent agent;
-
-    [SerializeField]
     private float speed = 5;
 
     [SerializeField]
+    private float rotationSpeed = 3;
+
+    [SerializeField]
     private InputActionReference moveAction;
+
+    [SerializeField]
+    private CharacterController characterController;
 
     private Vector3 motionVector;
 
@@ -29,19 +32,20 @@ public class Player : MonoBehaviour
         moveAction.action.performed += OnMove;
     }
 
-    private void Update()
-    {
-        if (motionVector != Vector3.zero)
-        {
-            transform.forward = motionVector;
-            agent.Move(motionVector * speed * Time.deltaTime);
-        }
-    }
 
     private void OnMove(InputAction.CallbackContext callbackContext)
     {
+        Debug.Log("On Move");
         Vector2 moveDelta = callbackContext.ReadValue<Vector2>();
-        motionVector = new Vector3(motionVector.x, transform.position.y, motionVector.y);
+        motionVector = transform.right * moveDelta.x + transform.forward * moveDelta.y;
+        characterController.Move(motionVector.normalized * speed * Time.deltaTime);
+    }
+
+    public void OnLook(Vector2 deltaInput)
+    {
+        Debug.Log("On look");
+
+        transform.Rotate(Vector3.up, deltaInput.x * rotationSpeed * Time.deltaTime);
     }
 
     private void OnDisable()
